2f4d48f56067c3de40715a592b30806ecf53485e
GabrielRCodes <gabrielrcodes@gmail.com>
2022-06-24 21:59:23 -0300

    TypeScript variables

---
+++ src/CreateCourseService.ts
@@ -0,0 +1,22 @@
+
+/* 
+
+  name - string
+  duration - number
+  educator - string
+
+*/
+
+interface Course {
+  name: string;
+  duration?: number; // Definindo atributo opcional com (?)
+  educator: string;
+}
+
+class CreateCourseService {
+  execute({ name, duration = 8, educator }: Course) {
+    console.log(name, duration, educator)
+  }
+}
+
+export default new CreateCourseService
